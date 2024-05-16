117a894674ada7e62da448e646bd8f4569d54617
Zirius <superrichardwang@yahoo.com>
2024-05-16 11:18:15 +0100

    Add commands to execute the whole project  in main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from model_training.train import *
+from model_training.LSTM import *
+from data_preprocess.preprocess import *
+from model_training.chatgpt_prompt import *
+from model_training.bag_of_word_linear_regression import *
 
-run_bert_sweep()
+#train_dataset,  val_dataset, test_dataset, vocab = preprocess_data()
+test_bag_of_words()
+#test_chatgpt(fraction=0.001,example_count=1,iterations=1,api_key="sk-proj-EwRsyEt1chy2ZSdlumsRT3BlbkFJqgPaeKVzUDyOawgAUFKH")
+
